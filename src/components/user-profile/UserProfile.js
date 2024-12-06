31221b498ca808ab1f0d19635faca1de753a2609
unknown <plamen.tsvetkov96@gmail.com>
2024-12-06 17:34:16 +0200

    fix user profile problem with fetching data

--- src/components/user-profile/UserProfile.js
+++ src/components/user-profile/UserProfile.js
@@ -1,17 +1,13 @@
 import { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
-import { useAuth } from "../../contexts/AuthContext";
 import { getDatabase, ref, get } from "firebase/database";
 import { RecipeContext } from "../../contexts/RecipeContext";
 import styles from "./UserProfile.module.css";
 
-
 export const UserProfile = () => {
     const [loading, setLoading] = useState(true);
     const [userData, setUserData] = useState(null);
-    const [recipeDetails, setRecipeDetails] = useState({});
 
-    const { currentUser: authUser } = useAuth();
     const { userId: commentUserId } = useParams();
     const { recipes } = useContext(RecipeContext);
 
@@ -30,20 +26,7 @@ export const UserProfile = () => {
                     console.error("User data not found in Realtime Database.");
                 }
 
-                if (authUser && authUser.uid === commentUserId) {
-                    fetchedUserData = {
-                        ...fetchedUserData,
-                        displayName: authUser.displayName,
-                        email: authUser.email,
-                        photoURL: authUser.photoURL,
-                    };
-                }
-
                 setUserData(fetchedUserData);
-
-                if (fetchedUserData.savedRecipes) {
-                    await fetchRecipeDetails(fetchedUserData.savedRecipes);
-                }
             } catch (error) {
                 console.error("Error fetching user data:", error);
             } finally {
@@ -51,36 +34,8 @@ export const UserProfile = () => {
             }
         };
 
-        const fetchRecipeDetails = async (savedRecipes) => {
-            try {
-                const db = getDatabase();
-                const recipePromises = Object.keys(savedRecipes).map(
-                    async (recipeId) => {
-                        const recipeRef = ref(db, `recipes/${recipeId}`);
-                        const recipeSnapshot = await get(recipeRef);
-                        if (recipeSnapshot.exists()) {
-                            return { id: recipeId, ...recipeSnapshot.val() };
-                        } else {
-                            console.warn(`Recipe with ID ${recipeId} not found.`);
-                            return null;
-                        }
-                    }
-                );
-
-                const fetchedRecipes = await Promise.all(recipePromises);
-                const recipesById = fetchedRecipes.reduce((acc, recipe) => {
-                    if (recipe) acc[recipe.id] = recipe;
-                    return acc;
-                }, {});
-
-                setRecipeDetails(recipesById);
-            } catch (error) {
-                console.error("Error fetching recipe details:", error);
-            }
-        };
-
         if (commentUserId) fetchUserData();
-    }, [commentUserId, authUser]);
+    }, [commentUserId]);
 
     if (loading) {
         return <div>Loading...</div>;
@@ -90,28 +45,33 @@ export const UserProfile = () => {
         return <div>User data not found.</div>;
     }
 
+    console.log(`Looking at user profile from comments: ${userData}`);
+    const totalRecipesCount = recipes.filter(
+        (r) => r.authorId === commentUserId
+    ).length;
+
     return (
         <div className={styles.profileContainer}>
             <div className={styles.backgroundImage}>
                 <div className={styles.userProfileHeader}>
                     <img
-                        src={userData.photoURL || "/default-avatar.png"}
-                        alt={`${userData.displayName || "User"}'s avatar`}
+                        src={userData.profilePicture || "/default-avatar.png"}
+                        alt={`${userData.username || "User"}'s avatar`}
                         className={styles.userPhoto}
                     />
                     <h2 className={styles.userName}>
-                        {userData.displayName || "Unknown User"}
+                        {userData.username || "Unknown User"}
                     </h2>
                     {/* to add location in user obj */}
-                    <p className={styles.userLocation}>San Francisco, CA</p>         
+                    <p className={styles.userLocation}>San Francisco, CA</p>
                     {/* ----- */}
                 </div>
             </div>
 
             <div className={styles.statsContainer}>
                 <div className={styles.statCard}>
-                    <h3>{recipes.filter((r) => r.authorId === commentUserId).length}</h3>
-                    <p>Recipes</p>
+                    <h3>{totalRecipesCount}</h3>
+                    <p> {totalRecipesCount > 1 ? "Recipes" : "Recipe"}</p>
                 </div>
                 <div className={styles.statCard}>
                     <h3>239</h3>
@@ -125,19 +85,30 @@ export const UserProfile = () => {
 
             <div className={styles.sectionsContainer}>
                 <section className={styles.section}>
-                    <h3>{userData.displayName.split(" ")[0]}'s Recipes</h3>
+                    <h3>All Recipes</h3>
                     <ul className={styles.recipeList}>
                         {recipes &&
                             recipes
                                 .filter((x) => x.authorId === commentUserId)
                                 .map((recipe) => (
-                                    <li key={recipe.id} className={styles.recipeItem}>
+                                    <li
+                                        key={recipe.id}
+                                        className={styles.recipeItem}
+                                    >
                                         <img
-                                            src={recipe.imageUrl || "/placeholder-recipe.png"}
+                                            src={
+                                                recipe.imageUrl ||
+                                                "/placeholder-recipe.png"
+                                            }
                                             alt={recipe.title || "Recipe Image"}
                                         />
-                                        <h4>{recipe.title || "Unknown Recipe"}</h4>
-                                        <p>{recipe.description || "No description available."}</p>
+                                        <h4>
+                                            {recipe.title || "Unknown Recipe"}
+                                        </h4>
+                                        <p>
+                                            {recipe.description ||
+                                                "No description available."}
+                                        </p>
                                     </li>
                                 ))}
                     </ul>
