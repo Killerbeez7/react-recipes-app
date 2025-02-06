b35a56ee85f243bcae04b2623855f9470510dad8
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-02-06 21:46:34 +0200

    change difficulty badges color

--- src/components/recipes/recipe-item/RecipeItem.js
+++ src/components/recipes/recipe-item/RecipeItem.js
@@ -2,22 +2,40 @@ import { Link } from "react-router-dom";
 import styles from "./RecipeItem.module.css";
 
 export const RecipeItem = ({ recipe }) => {
+    const difficulty = (recipe.difficulty || "Easy").toLowerCase();
     return (
         <div className={styles.recipeItem}>
-            <Link to={`/recipes/details/${recipe.id}`} className={styles.recipeLink}>
+            <Link
+                to={`/recipes/details/${recipe.id}`}
+                className={styles.recipeLink}
+            >
                 <div className={styles.recipeImageContainer}>
-                    {/* <div className={styles.difficultyBadge}>{recipe.difficulty || "Easy"}</div> */}
-                    <img src={recipe.imageUrl} alt={recipe.title} className={styles.recipeImage} />
+                    <div
+                        className={`${styles.difficultyBadge} ${styles[difficulty]}`}
+                    >
+                        {recipe.difficulty || "Easy"}
+                    </div>
+                    <img
+                        src={recipe.imageUrl}
+                        alt={recipe.title}
+                        className={styles.recipeImage}
+                    />
                 </div>
 
                 <div className={styles.recipeContent}>
                     <h3 className={styles.recipeTitle}>{recipe.title}</h3>
-                    <p className={styles.recipeDescription}>{recipe.description}</p>
+                    <p className={styles.recipeDescription}>
+                        {recipe.description}
+                    </p>
                     <div className={styles.recipeMeta}>
                         <span>
-                            <i class="fa-solid fa-clock" /> {recipe.timeToCook} min
+                            <i class="fa-solid fa-clock" /> {recipe.timeToCook}{" "}
+                            min
                         </span>
-                        <span><i class="fa-solid fa-burger"></i> {recipe.servings} servings</span>
+                        <span>
+                            <i class="fa-solid fa-burger"></i> {recipe.servings}{" "}
+                            servings
+                        </span>
                     </div>
                 </div>
             </Link>
